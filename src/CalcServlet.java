b0396193dc5097c1f315ba9dae4ce50436a87fc1
GrzegorzG19 <50138356+GrzegorzG19@users.noreply.github.com>
2019-07-12 16:54:18 +0200

    Update CalcServlet.java
    change /

--- src/CalcServlet.java
+++ src/CalcServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "CalcServlet")
+@WebServlet("/CalcServlet")
 public class CalcServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String metr = request.getParameter("metr");
